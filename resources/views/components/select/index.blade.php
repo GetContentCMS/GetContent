dfa3a10ae021626352e3b47f8ecd8a472ff82ddc
Rich Standbrook <richard@milk.media>
2022-02-03 16:57:45 +0000

    ✨ Adds select field component

---
+++ resources/views/components/select/index.blade.php
@@ -0,0 +1,27 @@
+@props([
+    'id' => null,
+])
+<x-gc::input-surround :attributes="$attributes->only(['id', 'label', 'help', 'error', 'class'])" :id="$id">
+
+    <div class="contents relative" x-data>
+        <select @class([
+        'appearance-none px-3 py-2 w-full rounded border shadow-sm focus-outline',
+        'text-gray-900',
+        'disabled:pointer-events-none disabled:opacity-60',
+
+        'border-red-300 text-red-900 focus:ring-red-500 focus:ring-opacity-50 focus:border-red-500' => $attributes->get('error'),
+
+        'dark:text-gray-100 dark:bg-gray-700 dark:border-gray-600',
+        'dark:ring-opacity-50 dark:disabled:bg-gray-800',
+        'dark:disabled:border-gray-600 dark:disabled:placeholder-gray-600',
+
+        'dark:border-red-500 dark:text-red-400  dark:focus:border-red-500' => $attributes->get('error'),
+
+        ]) {{$attributes->merge(['id' => $id])}} x-ref="select">
+            {{$slot}}
+        </select>
+        <x-heroicon-s-selector class="absolute right-2 top-3 w-5 h-5 dark:text-gray-100"
+                               ::class="{'opacity-40': $refs.select.disabled}"/>
+    </div>
+
+</x-gc::input-surround>
