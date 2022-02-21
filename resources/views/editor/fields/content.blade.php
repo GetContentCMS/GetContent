9a09d35e2e77e462a6847c4cb12e2ff10317228d
Rich Standbrook <richard@milk.media>
2022-02-21 16:42:33 +0000

    ✨ Adds given label to Content Field

--- resources/views/editor/fields/content.blade.php
+++ resources/views/editor/fields/content.blade.php
@@ -2,4 +2,9 @@
     $model = '$wire.entangle(\'' . $field->getModelPath('html') . '\').defer';
 @endphp
 
+@if($field->givenLabel)
+    <label
+        class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-200">{{$field->givenLabel}}</label>
+@endif
+
 <x-gc::writer :model="$model" class="w-full"/>
