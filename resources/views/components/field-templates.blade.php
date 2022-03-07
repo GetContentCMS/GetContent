09ffc6e87aeae5451c6786c61a34155c4f5f3524
Rich Standbrook <richard@milk.media>
2022-03-07 23:23:27 +0000

    Include all templates in field list

--- resources/views/components/field-templates.blade.php
+++ resources/views/components/field-templates.blade.php
@@ -1,5 +1,5 @@
-@foreach(GetContent::getFieldTemplates() as $template)
-    <x-gc::button wire:click="addField('template', {'template':  '{{$template->slug}}'})" flat class="h-12">
+@foreach(GetContent::getFieldTemplates(null) as $template)
+    <x-gc::button wire:click="addField('{{$template->type}}', {'template':  '{{$template->slug}}'})" flat class="h-12">
         <x-slot name="icon">
             <x-dynamic-component :component="$template->meta->icon ?? 'heroicon-s-dots-horizontal'"/>
         </x-slot>
